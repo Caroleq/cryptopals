4860ee33a31648f422d156b014e49606ddfa43a4
caroleq <asdsa>
2018-07-24 19:38:58 +0200

    fix

--- chall2.py
+++ chall2.py
@@ -1,18 +1,27 @@
 import binascii
 
-#extract raw data
-num1=binascii.unhexlify('1c0111001f010100061a024b53535009181c')
-num2=binascii.unhexlify('686974207468652062756c6c277320657965')
+
+def xor_hex(buffer1, buffer2):
+    """
+      Function takes two hex-encoded buffers of equal length and produces their xor value
+    """
+    if len(buffer1) != len(buffer2):
+        raise Exception("Buffers should have equal length")
+
+    binary1 = binascii.unhexlify(buffer1)
+    binary2 = binascii.unhexlify(buffer2)
+
+    xor_list = [ chr(val1^val2) for (val1, val2) in zip(binary1, binary2)]
+    xor = ''.join(xor_list)
+  
+    return str(binascii.hexlify( bytes(''.join(xor_list), 'utf-8')))[2:-1]
 
 
-out=""
-index=0
-#xor operation for whole number
-while index<len(num1):
-	out+=chr(ord(num1[index])^ord(num2[index]))
-	index+=1
-#hex encode
-encoded=binascii.hexlify(out)
-print encoded
+
+if __name__ == "__main__":
+    buffer1 = '1c0111001f010100061a024b53535009181c'
+    buffer2 = '686974207468652062756c6c277320657965'
+
+    assert xor_hex(buffer1, buffer2) == '746865206b696420646f6e277420706c6179'
 
 
