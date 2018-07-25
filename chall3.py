f4fb00d4dc68587b3f36d2ca0d2a2abae740177b
caroleq <karolina.gontarek20@gmail.com>
2018-07-25 19:33:42 +0200

    challenges 3,4

--- chall3.py
+++ chall3.py
@@ -1,45 +1,58 @@
 from __future__ import division
 import binascii
+from chall2 import xor_hex
 
-frequency_table={'E':1, 'e':1,
-		 'T':12/13, 't':12/13,
-		 'A':11/13, 'a':11/13,
-		 'O':10/13, 'o':10/13,
-		 'I':9/13, 'i':9/13, 
-		 'N':8/13, 'n':8/13,
-		 ' ':7/13, 
-		 'S':6/13, 's':6/13,
-		 'H':5/13, 'h':5/13,
-		 'R':4/13, 'r':4/13,
-		 'D':3/13, 'd':3/13,
-		 'L':2/13, 'l':2/13,
-		 'U':1/13, 'u':1/13}
+frequency_table = {	'E':1, 'e':1,
+		  		   	'T':12/13, 't':12/13,
+		   		   	'A':11/13, 'a':11/13,
+		   			'O':10/13, 'o':10/13,
+		   			'I':9/13, 'i':9/13, 
+		   			'N':8/13, 'n':8/13,
+		  			' ':7/13, 
+		   			'S':6/13, 's':6/13,
+		   			'H':5/13, 'h':5/13,
+		   			'R':4/13, 'r':4/13,
+		   			'D':3/13, 'd':3/13,
+		   			'L':2/13, 'l':2/13,
+		   			'U':1/13, 'u':1/13
+            	  }
 
-def break_xor_single_byte_by_freq(to_break):
-	"""test for single xor"""	
-	max_score_letter='\x00'
-	max_score=0
-	for char in range(0,256):
+def break_xor_single_byte_by_freq( to_break ):
+	"""
+	  Function tries to decrypt text (in hex format) xored with single string by using frequency table of characters (for English language)
+	  We choose ASCII code, where given text gets the highest score according to frequency table
+	"""	
+
+	to_break = binascii.unhexlify(to_break)
+
+	max_score_letter = '\x00'
+	max_score = 0
+
+	for char in range(256):
 		score=0.0
+
 		for letter in to_break:
-			c=chr(ord(letter)^char)
+
+			c = chr(letter^char)
 			if c in frequency_table.keys():
-				score+=frequency_table[c]
+				score += frequency_table[c]
+
 		if score > max_score:
-			max_score_letter=chr(char)
-			max_score=score
+			max_score_letter = chr(char)
+			max_score = score
+
+
 	return max_score_letter
 
-encoded='1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
-raw=binascii.unhexlify(encoded)
-#print raw
-break_=break_xor_single_byte_by_freq(raw)
 
-c=ord(break_)
-out=''
-for i in raw:
-	out+=chr(ord(i)^c)
+if __name__ == "__main__":
+    encoded='1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
 
-print out    
+    char = break_xor_single_byte_by_freq(encoded)
+
+    payload = binascii.hexlify( bytes(char*len(encoded),'ASCII') )[2:len(encoded)+2]
+
+    print( binascii.unhexlify(xor_hex(encoded ,payload)) )
+
 
 
--- chall4.py
+++ chall4.py
@@ -1,65 +1,65 @@
-from __future__ import division
-import binascii
+from  binascii import hexlify, unhexlify
 from operator import itemgetter
+from chall3 import  break_xor_single_byte_by_freq, frequency_table
+from chall2 import xor_hex
 
-frequency_table={'E':1, 'e':1,
-		 'T':12/13, 't':12/13,
-		 'A':11/13, 'a':11/13,
-		 'O':10/13, 'o':10/13,
-		 'I':9/13, 'i':9/13, 
-		 'N':8/13, 'n':8/13,
-		 ' ':7/13, 
-		 'S':6/13, 's':6/13,
-		 'H':5/13, 'h':5/13,
-		 'R':4/13, 'r':4/13,
-		 'D':3/13, 'd':3/13,
-		 'L':2/13, 'l':2/13,
-		 'U':1/13, 'u':1/13}
 
-def break_xor_single_byte_by_freq(to_break):
-	"""test for single xor"""	
-	max_score_letter='\x00'
-	max_score=0
-	for char in range(0,256):
-		score=0.0
-		for letter in to_break:
-			c=chr(ord(letter)^char)
-			if c in frequency_table.keys():
-				score+=frequency_table[c]
-		if score > max_score:
-			max_score_letter=chr(char)
-			max_score=score
-	return max_score_letter
 
-def find_line(data_table):
+def find_line(data_lines):
+	"""
+    Tries to break each line with single xor key. Then sorts list of 2-elem list [line, score] according to best score in frequency tables
+	"""
 	local_scores=[]
-	for line in data_table:
-		out=""
-		c=break_xor_single_byte_by_freq(line)
-		c=ord(c)
-		score=0
-		for letter in line:
-			l=chr(ord(letter)^c)
-			out+=l
-			if l in frequency_table.keys():
-				score+=frequency_table[l]
+	for line in data_lines:
 
-		local_scores.append([out,score])
-	local_scores.sort(key=itemgetter(1),reverse=True)
+		c = break_xor_single_byte_by_freq(line)
+		c = ord(c)
+
+		score = 0
+
+		unhexlified = unhexlify(line)
+		for letter in unhexlified:
+			l = chr(letter^c)
+
+			if l in frequency_table.keys():
+				score += frequency_table[l]
+
+		local_scores.append([line,score])
+
+
+	local_scores.sort(key=itemgetter(1), reverse=True)
 	return local_scores
 
 
-data_file=open('4.txt')
-hex_data=data_file.readlines()
-raw_data=[]
+if __name__ == "__main__":
+	"""
+      Solves problem from https://cryptopals.com/static/challenge-data/4.txt
+	"""
 
-for line in hex_data:
-	raw_data.append(binascii.unhexlify(line.rstrip('\n')))
 
-sc=find_line(raw_data)
-print sc[0][0]
+	data_file = open('4.txt')
+	hex_data = data_file.readlines()
+	raw_data = []
 
-data_file.close()
+	for line in hex_data:
+		raw_data.append(line.rstrip('\n'))
+
+	sorted_lines = find_line(raw_data)
+
+	best = sorted_lines[0][0]
+
+	char =  break_xor_single_byte_by_freq( best ) 	
+	payload = hexlify( bytes(char*len( best ),'ASCII') )[2:len( best )+2]
+
+	print( unhexlify(xor_hex( best ,payload)) )
+
+	data_file.close()
+
+
+
+
+
+
 
 
 
