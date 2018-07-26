34c77cba41b6e5544070f84726de8573813ea499
caroleq <karolina.gontarek20@gmail.com>
2018-07-26 09:06:46 +0200

    challenge 5

--- chall5.py
+++ chall5.py
@@ -1,23 +1,36 @@
-import binascii
+from  binascii import hexlify
+from chall2 import xor_hex
 
 
-def encrypt_xor(text,key):
-	i=0
-	out=""
-	for letter in text:
-		out+=chr(ord(key[i])^ord(letter))
-		i+=1
-		if i==len(key):
-			i=0
-	return out
+def encrypt_xor( plaintext, key):
+	"""
+		XORS plaintext with repeted key using function from challenge 2.
+		Splits plaintext on every '\n' char
+	"""
 
-def main():
-	to_encrypt="Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
-	key="ICE"
-	print binascii.hexlify(encrypt_xor(to_encrypt,key))
+	lines = plaintext.split('\n')
+
+
+	xor = ""
+	for line in lines:
+
+		to_multiply = int(len(line)/len(key))+1
+
+		hexlified1 = str( hexlify( bytes( line, 'utf-8' ) ) )[2:-1]
+		hexlified2 = str( hexlify( bytes(key*to_multiply, 'utf-8')) )[2:len(hexlified1)+2]	
+	
+		xor += xor_hex(hexlified1, hexlified2) + '\n'
+	
+
+	return xor
+
 
 
 if __name__=="__main__":
-	main()
+	
+	secret = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+	key = "ICE"
+
+	print ( encrypt_xor(secret, key), end='' )
 
 
