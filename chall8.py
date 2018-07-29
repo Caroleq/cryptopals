6ae676777b637d2ae108397a710f300d572e583f
caroleq <karolina.gontarek20@gmail.com>
2018-07-29 19:21:51 +0200

    challenge 8

--- chall8.py
+++ chall8.py
@@ -1,36 +1,48 @@
-import binascii
+from binascii import unhexlify, hexlify, b2a_hex
 
-def detect_ECB(data):
-	"""detects ECB encrypted string because (16 bytes length blocks)"""
-	encrypted=[]
-	for line in data:
-		split=[line[i:i+16] for i in xrange(0, len(line),16)]
-		j=0
-		while j< len(split):	
-			k=j+1
-			while k<len(split):
-				if split[j]==split[k]:				#if block is repeated, then it was probably encrypted in ecb mode
-					encrypted.append(line)			#so we can add this line to the list and stop searching
-					break
-				k+=1
-			if line in encrypted:
-				break
-			j+=1
-	return encrypted
+class FoundECBException(Exception):
+	pass
 
+def detect_AES_ECB( data_lines, block_size=16 ):
+	"""
+		detects indexes of line encrypted in ECB hint from the challenge: 'the same 16 byte plaintext block will always produce the same 16 byte ciphertext.''
 
-def main():
-	data_f=open('8.txt')
-	data_lines=data_f.readlines()	
-	data=[]
-	for line in data_lines:
-		data.append(binascii.unhexlify(line.strip('\n')))
-	e=detect_ECB(data)
-	for elem in e:
-		print binascii.hexlify(elem)
-	data_f.close()
+		ECB_lines stores positions of detected strings
+	"""
+
+	ECB_lines = []
+
+	for line_index, line in enumerate(data_lines):
+
+		blocks = [ line[i:i+block_size] for i in range(0, len(line),block_size) ]
+
+		try:
+			for index1 in range(len(blocks)):
+				for index2 in range(index1+1, len(blocks)):
+					if blocks[index1] == blocks[index2]:
+						ECB_lines.append(line_index)
+						raise FoundECBException
+
+		except FoundECBException as e:
+			pass
+
+	return ECB_lines
 
 
-if __name__=="__main__":
-	main()
 
+
+
+if __name__ == "__main__":
+	"""
+	  	Finds lines encrypted in AES ECB mode in https://cryptopals.com/static/challenge-data/8.txt
+		Prints detected strings with line number
+	"""
+
+	data_lines = open('8.txt').readlines()
+
+	raw_lines = [ str(unhexlify( line.strip('\n') ))  for line in data_lines ]
+
+	indexes = detect_AES_ECB(raw_lines)
+
+	for index in indexes:
+		print("ECB mode detected in", index+1, "line, ciphertext :", data_lines[index].strip("\n") )
