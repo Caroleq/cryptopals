61e8829aad68789a213d8eef5c65c4a8fb86228e
caroleq <karolina.gontarek20@gmail.com>
2018-07-29 19:36:25 +0200

    challenge 9

--- chall9.py
+++ chall9.py
@@ -1,16 +1,26 @@
-import binascii
+
 
 def pad_pkcs7(text, num=16):
-	"""padding to multiply of num"""
-	to_pad=num-len(text)%num
-	text+=chr(to_pad)*to_pad
+	"""
+		Data are padded  so after function execution len(text)%num == 0
+		If  in original text len(text)%num == 0, additial padding block is added
+
+		Padding value is chr(num - len(text)%num)
+	"""
+
+	padding_length = num - len(text)%num
+	padding_value = chr(padding_length)
+
+	text += padding_value*padding_length
+
 	return text
 
-def main():
-	text="YELLOW SUBMARINE"
-	padded=pad_pkcs7(text)
-	print padded
+
 
 if __name__=="__main__":
-	main()
+	"""
+		Example from challenge
+	"""
+
+	assert pad_pkcs7("YELLOW SUBMARINE", 20) == "YELLOW SUBMARINE\x04\x04\x04\x04"
 
