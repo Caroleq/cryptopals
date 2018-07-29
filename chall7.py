66dc298558113ec13d02605a7a651245b44ae04a
caroleq <karolina.gontarek20@gmail.com>
2018-07-29 18:18:48 +0200

    challenge 7

--- chall7.py
+++ chall7.py
@@ -1,20 +1,31 @@
 from Crypto.Cipher import AES
 from Crypto import Random
-import base64
+from base64 import b64decode
 
-def main():
-	key=b'YELLOW SUBMARINE'
-	cipher=AES.new(key,AES.MODE_ECB)
-	
-	data_f=open('7.txt')
-	b64data=data_f.read()
-	data=base64.b64decode(b64data)
-	encrypted=cipher.decrypt(data)
-	print encrypted
+
+def AES_ECB_decrypt(encrypted, key):
+	"""
+		decrypts text encrypted in AES ECB mode using key
+	"""
+
+	cipher = AES.new( key, AES.MODE_ECB)
+	decrypted = cipher.decrypt( encrypted )
+
+	return decrypted
+
+
 
 
 if __name__=="__main__":
-	main()
+	"""
+		file decryption from https://cryptopals.com/static/challenge-data/7.txt
+	"""
+	data = open('7.txt').read()
+	raw = b64decode(data)
+
+	key = b'YELLOW SUBMARINE'
+
+	print( AES_ECB_decrypt(raw, key).decode('utf-8') )
 
 
 
